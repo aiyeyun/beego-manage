a01f3c9c5d55e3755662731cfafe4459668f94cc
王海波 <644362887@qq.com>
2017-09-23 20:00:00 +0800

    路由

--- routers/router.go
+++ routers/router.go
@@ -40,7 +40,7 @@ func init() {
 	//管理员列表
 	beego.Router("/admin/index/?:p([0-9])+", &controllers.AdminController{}, "get:Users")
 	//添加管理员
-	beego.Router("/admin/Form", &controllers.AdminController{}, "*:Form")
+	beego.Router("/admin/form", &controllers.AdminController{}, "*:Form")
 	//角色列表
 	beego.Router("/admin/role", &controllers.AdminController{})
 	//角色授权
